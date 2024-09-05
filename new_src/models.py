6e6fc82f5e4b21c9653e7597804337bc9e755381
hagerrady13 <hagerrady133@gmail.com>
2024-09-05 15:34:12 -0600

    re-training maskedMLP with one-hot encoding and C-tran

--- new_src/models.py
+++ new_src/models.py
@@ -38,16 +38,31 @@ class MaskedMLP(nn.Module):
         backbone=None,
         attention_layers=2,
         heads=2,
+        num_unique_mask_values=3,
     ):
         super(MaskedMLP, self).__init__()
-        self.layer_1 = nn.Linear(input_channels + num_classes, d_hidden)
-        self.layer_2 = nn.Linear(d_hidden, d_hidden)
-        self.layer_3 = nn.Linear(d_hidden, num_classes)
+        self.out_layer = nn.Linear(d_hidden*2, num_classes)
+
+        self.num_unique_mask_values = num_unique_mask_values
+
+        self.env_encoder = nn.Sequential(
+            nn.Linear(input_channels, d_hidden),
+            nn.ReLU(),
+            nn.Linear(d_hidden, d_hidden)
+        )
+        self.mask_encoder = nn.Sequential(
+            nn.Linear(self.num_unique_mask_values * num_classes, d_hidden),
+            nn.ReLU(),
+            nn.Linear(d_hidden, d_hidden)
+        )
 
     def forward(self, x, mask):
-        x_combined = torch.cat((x, mask), dim=1)
+        mask = mask.long()
+        one_hot_mask = F.one_hot(mask, num_classes=self.num_unique_mask_values).float()  # One-hot encoding
+        one_hot_mask_flattened = one_hot_mask.view(mask.size(0), -1)  # Flatten to (batch_size, num_classes * 3)
+        mask_encoded = self.mask_encoder(one_hot_mask_flattened)
 
-        x = F.relu(self.layer_1(x_combined))
-        x = F.relu(self.layer_2(x))
-        x = self.layer_3(x)
+        x_encoded = self.env_encoder(x)
+        x_combined = torch.cat((x_encoded, mask_encoded), dim=1)
+        x = self.out_layer(x_combined)
         return x
