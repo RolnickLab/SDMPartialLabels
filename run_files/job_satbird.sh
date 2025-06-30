fe9716c184af8abefac015da37017e9cc199c286
hagerrady13 <hagerrady133@gmail.com>
2025-06-30 11:50:49 -0400

    update .gitignore

---
+++ run_files/job_satbird.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=satbird_ciso
+#SBATCH --output=job_output_test.txt
+#SBATCH --error=job_error_test.txt
+#SBATCH --ntasks=1
+#SBATCH --time=23:59:00
+#SBATCH --mem-per-cpu=50Gb
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
+
+module load miniconda/3
+conda activate new_env
+export COMET_API_KEY=$COMET_API_KEY
+python train.py args.config=configs/satbird/satbird_ciso.yaml
